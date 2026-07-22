1d93acef2538eb2ed8d7ec08d114fc91398dd21d
Your Name <you@example.com>
2026-07-22 10:46:28 +0530

    changes for env files

--- backend/src/server.js
+++ backend/src/server.js
@@ -50,7 +50,7 @@ connectDB();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser()); 
+app.use(cookieParser());
 app.use(globalLimiter);
 
 // Routes
